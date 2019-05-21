5d32a8583322e1426bff38715b4afb25dd2738d7
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-05-21 10:15:31 +0200

    Finalized appearance etc.

--- Web_Base_Haz_app.R
+++ Web_Base_Haz_app.R
@@ -16,8 +16,24 @@ ui <- fluidPage(
     tags$head(
         tags$style(
             HTML("
+            
+            input#Intercept1.form-control.shiny-bound-input {
+                background-color: #1D7BFF;
+                color: #FFFFFF;
+                font-weight: bold;
+                border: #FFFFFF;
+                border-style: solid;
+                border-width: 5px;
+            }
+            
+            input#Phi1.form-control.shiny-bound-input {
+                background-color: #1D7BFF;
+                color: #FFFFFF;
+                font-weight: bold;
+            }
+            
             input#Intercept2.form-control.shiny-bound-input {
-                background-color: #FF265C;
+                background-color: #EB2143;
                 color: #FFFFFF;
                 font-weight: bold;
                 border: #FFFFFF;
@@ -26,7 +42,22 @@ ui <- fluidPage(
             }
             
             input#Phi2.form-control.shiny-bound-input {
-                background-color: #FF265C;
+                background-color: #EB2143;
+                color: #FFFFFF;
+                font-weight: bold;
+            }
+            
+            input#Intercept3.form-control.shiny-bound-input {
+                background-color: #00BA38;
+                color: #FFFFFF;
+                font-weight: bold;
+                border: #FFFFFF;
+                border-style: solid;
+                border-width: 5px;
+            }
+            
+            input#Phi3.form-control.shiny-bound-input {
+                background-color: #00BA38;
                 color: #FFFFFF;
                 font-weight: bold;
             }
@@ -49,37 +80,37 @@ ui <- fluidPage(
                HTML("<hr style = 'border-color: #FFFFFF; border-width: 1px; 
                            margin: 5px 5px; border-style: solid' >"), 
                numericInput('Intercept1', tags$span(style = 'color : #FFFFFF', 'Intercept'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5), 
+                            min = -100, max = 100, value = 0, step = 0.5), 
                br(), 
                numericInput('Phi1', tags$span(style = 'color : #FFFFFF', 'Phi'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5)
+                            min = 0, max = 100, value = 0, step = 0.1)
                )), 
         column(3, div(style = 'height: 200px; background-color : #EB2143;',
                tags$span(style = 'color: #FFFFFF; font-weight: bold; font-size: 18px', 'Transition 2'), 
                HTML("<hr style = 'border-color: #FFFFFF; border-width: 1px; 
                            margin: 5px 5px; border-style: solid' >"),
                numericInput('Intercept2', tags$span(style = 'color: #FFFFFF', 'Intercept'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5), 
+                            min = -100, max = 100, value = 0, step = 0.5), 
                br(), 
                numericInput('Phi2', tags$span(style = 'color: #FFFFFF', 'Phi'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5))
+                            min = 0, max = 100, value = 0, step = 0.1))
         ), 
-        column(3, div(style = 'height: 200px; background-color: #FF265C;',
+        column(3, div(style = 'height: 200px; background-color: #00BA38;',
                tags$span(style = 'color: #FFFFFF; font-weight: bold; font-size: 18px', 'Transition 3'), 
                HTML("<hr style = 'border-color: #FFFFFF; border-width: 1px; 
                            margin: 5px 5px; border-style: solid' >"),
                numericInput('Intercept3', tags$span(style = 'color: #FFFFFF', 'Intercept'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5), 
+                            min = -100, max = 100, value = 0, step = 0.5), 
                br(), 
                numericInput('Phi3', tags$span(style = 'color: #FFFFFF', 'Phi'), 
-                            min = -1000, max = 1000, value = 0, step = 0.5)
+                            min = 0, max = 100, value = 0, step = 0.1)
         )), 
         column(3, div(style = 'height: 200px; background-color: #000000;', 
                       tags$span(style = 'color: #FFFFFF; font-weight: bold; font-size: 18px', 'Time Range'), 
                       HTML("<hr style = 'border-color: #FFFFFF; border-width: 1px; 
                            margin: 5px 5px; border-style: solid' >"), 
                sliderInput('time', tags$span(style = 'color: #FFFFFF', 'time'), 
-                           min = 0, max = 1000, value = c(0, 12))
+                           min = 0.001, max = 100, value = c(0, 12))
         ))
     )
 )
@@ -87,16 +118,16 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    x <- reactive(seq(input$time[[1]], input$time[[2]], length.out = 1000))
+    x <- reactive(seq(input$time[1], input$time[2], length.out = 5))
     y1 <- reactive(exp(input$Intercept1) * input$Phi1 * (x()^(input$Phi1 - 1)))
     y2 <- reactive(exp(input$Intercept2) * input$Phi2 * (x()^(input$Phi2 - 1)))
     y3 <- reactive(exp(input$Intercept3) * input$Phi3 * (x()^(input$Phi3 - 1)))
 
     output$plot <- renderPlot({
         
-        ggplot() + geom_line(aes(x = x(), y = y1()), color = '#000000') + 
+        ggplot() + geom_line(aes(x = x(), y = y1()), color = '#1D7BFF') + 
             geom_line(aes(x = x(), y = y2()), color = '#EB2143') + 
-            geom_line(aes(x = x(), y = y3()), color = '#3A54FF') + 
+            geom_line(aes(x = x(), y = y3()), color = '#00BA38') + 
             theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
             xlab('Time') + ylab("Baseline Hazard")
         
